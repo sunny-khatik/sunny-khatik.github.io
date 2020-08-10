d5925e5f0bb37e936befab304e696d4b91657062
sunny khatik <61911564+sunny-khatik@users.noreply.github.com>
2020-08-10 19:56:49 +0530

    js

--- js/js_signup.js
+++ js/js_signup.js
@@ -1,12 +1,26 @@
 
 // Validating Empty Field
-function check_empty() {
-	if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('psw_1').value == "" ||document.getElementById('psw_2').value == "") {
+function check_empty() 
+{
+	if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('psw_1').value == "" ||document.getElementById('psw_2').value == "") 
+	{
 		alert("Fill All Fields !");
-	} else {
-		document.getElementById('form').submit();
-		alert("Data added successfuly now you redirected to login page.");
-	}
+	} 
+	else 
+	{
+		var pass1 = document.getElementById('psw_1').value;
+		var pass2 = document.getElementById('psw_2').value
+		if (pass1 != pass2) 
+		{ 
+        	alert ("\nPassword are not matching try again ...!");
+			
+		}
+		else
+		{ 
+			document.getElementById('form').submit();
+			alert("Data added successfuly now you redirected to login page.");
+		}
+    } 
 }
 //Function To Display Popup
 function div_show()
